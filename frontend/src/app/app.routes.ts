0d9716c61c8913f61a4ddc93014e8f9cbf57613b
filipemoura <filipemouradev@gmail.com>
2026-01-13 15:29:15 -0300

    feat(frontend): adicionado interceptor JWT para autenticação automática

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent},
 
-    {path:'', redirectTo:'login', pathMatch:'full'}
+    {
+        path: '',
+        canActivate: [authGuard],
+        children: [
+            //incluir rotas privadas aqui
+        ]
+    },
+
+    { path: '**', redirectTo: 'login' },
 ];
