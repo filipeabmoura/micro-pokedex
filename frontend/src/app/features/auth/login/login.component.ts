4c3fc5d892b671c9c2f9457f84bf7b6057afe756
filipemoura <filipemouradev@gmail.com>
2026-01-13 10:02:50 -0300

    implementado login integrado ao backend

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  error = '';
 
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ){}
+
+  submit(){
+    this.authService.login(this.email, this.password).subscribe({
+      next: () => this.router.navigate(['/']), error: () => (this.error = 'Credenciais inválidas')
+    });
+  }
 }
