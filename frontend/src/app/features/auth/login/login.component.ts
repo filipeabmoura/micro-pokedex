7d4a29d3669084113c354830ee212f35c4e068ba
filipemoura <filipemouradev@gmail.com>
2026-01-13 13:01:12 -0300

    refactor(frontend): componentes standalone

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -20,6 +23,6 @@ export class RegisterComponent {
   submit(){
     this.authService.register(this.email, this.password).subscribe(() => {
       this.router.navigate(['/login']);
-    });
+    })
   }
 }
