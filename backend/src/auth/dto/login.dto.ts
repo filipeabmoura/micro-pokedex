242a63effb81e60f2805106e8afece2864d30597
filipemoura <filipemouradev@gmail.com>
2026-01-21 16:07:25 -0300

    fix(backend): campos de cadastro e login de usuario validados corretamente

--- backend/src/auth/dto/login.dto.ts
+++ backend/src/auth/dto/login.dto.ts
@@ -1,4 +1,13 @@
+import { IsEmail, IsNotEmpty, MaxLength, MinLength } from 'class-validator';
+
 export class LoginDto {
-    email: string;
-    password: string;
+
+  @IsEmail({}, { message: 'Email inválido' })
+  @IsNotEmpty({ message: 'Email é obrigatório' })
+  email: string;
+
+  @IsNotEmpty({ message: 'Senha é obrigatória' })
+  @MinLength(6, { message: 'Senha deve ter no mínimo 6 caracteres' })
+  @MaxLength(12, { message: 'Senha deve ter no máximo 12 caracteres' })
+  password: string;
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -11,6 +12,14 @@ async function bootstrap() {
     origin:'http://localhost:4200',
     credentials: true,
   })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,            // remove campos extras
+      forbidNonWhitelisted: true, // erro se enviar campo não permitido
+      transform: true,
+    }),
+  );
 
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
